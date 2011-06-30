4db96540982d738775e02f93fea00dea491fc094
Maurizio De Santis <desantis.maurizio@gmail.com>
2011-06-30 16:34:49 +0200

    0.1.alpha.2

--- lib/rfql.rb
+++ lib/rfql.rb
@@ -1,7 +1,7 @@
 require 'rfql/query'
 require 'rfql/request'
 module RFQL
-  VERSION = '0.1.alpha'
+  VERSION = '0.1.alpha.2'
   FQLURL = "https://api.facebook.com/method/fql.query"
   def self.request(obj = nil)
     RFQL::Request.new(obj)
